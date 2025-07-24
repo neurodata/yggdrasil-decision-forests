9f048c27e607a15e1c9efcbbdac6fa957e18e8d0
Ariel Lubonja <ariel@cs.jhu.edu>
2025-07-23 21:24:51 -0400

    Fixed incorrect tree_depth arg name in bash xp

--- benchmarks/src/bash_scripts/exact_vs_histogram_multithr_4_depth.sh
+++ benchmarks/src/bash_scripts/exact_vs_histogram_multithr_4_depth.sh
@@ -7,7 +7,8 @@ python3 benchmarks/src/data_size_vs_train_time.py \
     --repeats=1 \
     --rows_list 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576\
     --cols=1024 \
-    --threads=48 ;
+    --threads=48 \
+    --tree_depth=4;
     
     
 python3 benchmarks/src/data_size_vs_train_time.py\
@@ -16,4 +17,5 @@ python3 benchmarks/src/data_size_vs_train_time.py\
     --numerical_split_type="Random"\
     --experiment_name="Oblique Random Histogram - Row Scaling - 48thr"\
     --threads=48\
-    --repeats=1
+    --repeats=1\
+    --tree_depth=4
