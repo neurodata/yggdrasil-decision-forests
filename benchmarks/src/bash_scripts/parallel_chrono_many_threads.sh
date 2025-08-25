b4475ecda5e9f6c2d84e701e7ee107262ac9f3c2
Ariel Lubonja <ariel@cs.jhu.edu>
2025-08-25 17:26:28 -0400

    Change parallel_chrono to Trunk CSV

--- benchmarks/src/bash_scripts/parallel_chrono_many_threads.sh
+++ benchmarks/src/bash_scripts/parallel_chrono_many_threads.sh
@@ -7,16 +7,16 @@ while true; do sudo -n true ; sleep 60 ; done &
 SUDO_KEEPALIVE_PID=$!
 
 # So it's found when script is run as sudo
-benchmarks/.venv/bin/python benchmarks/src/parallel_chrono.py --num_threads=32 --rows=10000 --tree_depth=8 --num_trees=64
+benchmarks/.venv/bin/python benchmarks/src/parallel_chrono.py --num_threads=32  --input_csv=benchmarks/data/trunk_data/10000x4096.csv --target_col=Target --tree_depth=8 --num_trees=64
 
-benchmarks/.venv/bin/python benchmarks/src/parallel_chrono.py --num_threads=16 --rows=10000 --tree_depth=8 --num_trees=64
+benchmarks/.venv/bin/python benchmarks/src/parallel_chrono.py --num_threads=16 --input_csv=benchmarks/data/trunk_data/10000x4096.csv --target_col=Target --tree_depth=8 --num_trees=64
 
-benchmarks/.venv/bin/python benchmarks/src/parallel_chrono.py --num_threads=8 --rows=10000 --tree_depth=8 --num_trees=64
+benchmarks/.venv/bin/python benchmarks/src/parallel_chrono.py --num_threads=8 --input_csv=benchmarks/data/trunk_data/10000x4096.csv --target_col=Target --tree_depth=8 --num_trees=64
 
-benchmarks/.venv/bin/python benchmarks/src/parallel_chrono.py --num_threads=4 --rows=10000 --tree_depth=8 --num_trees=64
+benchmarks/.venv/bin/python benchmarks/src/parallel_chrono.py --num_threads=4 --input_csv=benchmarks/data/trunk_data/10000x4096.csv --target_col=Target --tree_depth=8 --num_trees=64
 
-benchmarks/.venv/bin/python benchmarks/src/parallel_chrono.py --num_threads=2 --rows=10000 --tree_depth=8 --num_trees=64
+benchmarks/.venv/bin/python benchmarks/src/parallel_chrono.py --num_threads=2 --input_csv=benchmarks/data/trunk_data/10000x4096.csv --target_col=Target --tree_depth=8 --num_trees=64
 
-benchmarks/.venv/bin/python benchmarks/src/parallel_chrono.py --num_threads=1 --rows=10000 --tree_depth=8 --num_trees=64
+benchmarks/.venv/bin/python benchmarks/src/parallel_chrono.py --num_threads=1 --input_csv=benchmarks/data/trunk_data/10000x4096.csv --target_col=Target --tree_depth=8 --num_trees=64
 
 kill "${SUDO_KEEPALIVE_PID}"
